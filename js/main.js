81e709cb293cdd586d0b85b9a4296b50f139ec88
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2025-08-04 21:39:38 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-function controlRemote() {
+function controlDOMColor() {
     const header = document.querySelector("header")
     const isChecked = header.querySelector("#remote")
     const remoteDisplay = document.querySelector(".remote-display")
+    const sectionHeight = document.querySelector("section").offsetHeight
 
     isChecked.addEventListener("change", () => {
         if (isChecked.checked) {
@@ -11,7 +12,14 @@ function controlRemote() {
             remoteDisplay.classList.remove("activate")
             header.classList.remove("activate")
         }
+
+        if (scrollY >= sectionHeight) header.classList.add("activate")
+    })
+    
+    window.addEventListener("scroll", () => {
+        if (scrollY >= sectionHeight) header.classList.add("activate")
+        else header.classList.remove("activate")
     })
 }
 
-controlRemote()
+controlDOMColor()
