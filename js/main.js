069207b666e38c7e9d08129031992dc1edea02db
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2025-08-05 18:52:23 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+import LocomotiveScroll from 'https://esm.sh/locomotive-scroll'
+
+function controlDOMColor() {
+    const header = document.querySelector("header")
+    const isChecked = header.querySelector("#remote")
+    const remoteDisplay = document.querySelector(".remote-display")
+    const sectionHeight = document.querySelector("section").offsetHeight
+
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth: true,
+      lerp: 0.08,
+    })
+
+    scroll.on("scroll", (args) => {
+        if (isChecked.checked) return
+        if (args.scroll.y >= sectionHeight) header.classList.add("activate")
+        else header.classList.remove("activate")
+    })
+
+    isChecked.addEventListener("change", () => {
+        if (isChecked.checked) {
+            remoteDisplay.classList.add("activate")
+            header.classList.add("activate")
+        } else {
+            remoteDisplay.classList.remove("activate")
+            header.classList.remove("activate")
+            if (scroll.scroll.instance.scroll.y >= sectionHeight) header.classList.add("activate")
+            else header.classList.remove("activate")
+        }
+    })
+    
+    window.addEventListener("scroll", () => {
+        if (scrollY >= sectionHeight) header.classList.add("activate")
+        else header.classList.remove("activate")
+    })
+}
+
+controlDOMColor()
