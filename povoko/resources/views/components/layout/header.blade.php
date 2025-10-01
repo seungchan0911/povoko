55d47b42596f6754872a3208df7925dadd129950
seungchan0911 <imrealsexy00@gmail.com>
2025-10-01 13:58:16 +0900

    add laravel

---
+++ povoko/resources/views/components/layout/header.blade.php
@@ -0,0 +1,18 @@
+<img class="background-image" src="./img/source/background.png" alt="">
+<header>
+    <input type="checkbox" id="remote" hidden>
+    <label for="remote"><div class="remote">pv<br>ko</div></label>
+</header>
+<div class="remote-display">
+    <ul>
+        <li>
+            <a href="{{ route('index') }}">home</a>
+        </li>
+        <li>
+            <a href="{{ route('works') }}">works</a>
+        </li>
+        <li>
+            <a href="{{ route('index') }}">contact</a>
+        </li>
+    </ul>
+</div>
---
+++ povoko/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'index')->name('index');
+Route::view('works', 'works')->name('works');
