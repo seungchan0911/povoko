a3455602c9fe0922d2b729cd36c421709d3c0031
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2025-11-23 22:23:07 +0900

    hi

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -4,18 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Admin;
-use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Admin::create([
-            'password' => Hash::make('Alex0422!!'),
-            'name' => '관리자',
-        ]);
+        // 기존 admin이 없을 때만 생성
+        if (Admin::count() === 0) {
+            Admin::create([
+                'password' => 'Alex0422!!', // casts에서 자동 해시됨
+            ]);
+        }
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TextSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
