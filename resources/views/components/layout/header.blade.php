b6b72532d52faf00ab82146f730613e08f5d4394
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2025-11-24 22:56:29 +0900

    hi

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,7 +1,20 @@
 <img class="background-image" src="{{ asset('img/source/background.png') }}" alt="">
 <header>
     <input type="checkbox" id="remote" hidden>
-    <label for="remote"><div class="remote">pv<br>ko</div></label>
+    <label for="remote"><div class="remote">
+    <svg width="34" height="24" viewBox="0 0 34 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_314_11)">
+    <rect width="34" height="1.25" fill="black"/>
+    <rect width="34" height="1.25" transform="translate(0 11.375)" fill="black"/>
+    <rect width="34" height="1.25" transform="translate(0 22.75)" fill="black"/>
+    </g>
+    <defs>
+    <clipPath id="clip0_314_11">
+    <rect width="34" height="24" fill="white"/>
+    </clipPath>
+    </defs>
+    </svg>
+    </div></label>
 </header>
 <div class="remote-display">
     <ul>
