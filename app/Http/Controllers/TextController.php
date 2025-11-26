fcd676ba67c2fc9a7d59e2387a4ecfe63de2f00f
seungchan0911 <167081641+seungchan0911@users.noreply.github.com>
2025-11-26 22:11:21 +0900

    Add debug logs to form-autosave and improve edit page handling

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -50,41 +50,20 @@ class TextController extends Controller
         $request->validate([
             'text1' => 'required',
             'text2' => 'required',
-            'background_video_1' => 'nullable|file|mimes:mp4,mov,avi,wmv,webm|max:204800',
-            'background_video_2' => 'nullable|file|mimes:mp4,mov,avi,wmv,webm|max:204800',
-            'background_video_3' => 'nullable|file|mimes:mp4,mov,avi,wmv,webm|max:204800',
-            'background_video_4' => 'nullable|file|mimes:mp4,mov,avi,wmv,webm|max:204800',
+            'background_video_1' => 'nullable|url',
+            'background_video_2' => 'nullable|url',
+            'background_video_3' => 'nullable|url',
+            'background_video_4' => 'nullable|url',
         ]);
         
         $data = [
             'text1' => $request->text1,
             'text2' => $request->text2,
+            'background_video_1' => $request->background_video_1,
+            'background_video_2' => $request->background_video_2,
+            'background_video_3' => $request->background_video_3,
+            'background_video_4' => $request->background_video_4,
         ];
-        
-        // 4개의 비디오 처리
-        for ($i = 1; $i <= 4; $i++) {
-            $fieldName = "background_video_{$i}";
-            
-            if ($request->hasFile($fieldName)) {
-                $file = $request->file($fieldName);
-                
-                if ($file->isValid()) {
-                    // 기존 파일 삭제
-                    if ($text->$fieldName && \Storage::disk('public')->exists($text->$fieldName)) {
-                        \Storage::disk('public')->delete($text->$fieldName);
-                    }
-                    
-                    // 새 파일 저장
-                    $data[$fieldName] = $file->store('videos', 'public');
-                } else {
-                    // 파일이 유효하지 않으면 기존 값 유지
-                    $data[$fieldName] = $text->$fieldName;
-                }
-            } else {
-                // 파일이 업로드되지 않았으면 기존 값 유지
-                $data[$fieldName] = $text->$fieldName;
-            }
-        }
     
         $text->update($data);
 
